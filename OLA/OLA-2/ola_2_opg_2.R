70ee1d8baa2386c16f0050a68b574105721c0306
Olmand999 <ether-tapping-2w@icloud.com>
2026-09-20 22:48:49 +0200

    Late night sesh

--- OLA/OLA-2/ola_2_opg_2.R
+++ OLA/OLA-2/ola_2_opg_2.R
@@ -1,1 +1,25 @@
 # Opg. 2.1
+alltable = dkstat::dst_get_tables()
+
+forv1_meta <- dst_meta("FORV1", lang = "da")
+forv1_meta$variables 
+forv1_meta$values$INDIKATOR
+
+forv1 <- dst_get_data("FORV1",
+                      query = list(INDIKATOR = "*", Tid = "*"),
+                      lang = "da", meta_data = forv1_meta)
+
+colnames(forv1) <- c("indikator", "tid", "nettotal")
+forv1$kode <- sub(" .*", "", forv1$indikator)
+
+## Hvilke år har data for alle måneder
+maanedlig_check <- aggregate(!is.na(nettotal) ~ format(tid, "%Y"),
+                             data = forv1[forv1$indikator == "F1 Forbrugertillidsindikatoren", ],
+                             FUN = sum)
+names(maanedlig_check) <- c("aar", "antal_maaneder_med_data")
+maanedlig_check
+
+## Fjern data fra før 1996
+forv1_96 <- subset(forv1, tid >= as.Date("1996-01-01"))
+
+
