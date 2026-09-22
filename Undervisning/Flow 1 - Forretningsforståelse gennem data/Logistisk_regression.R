4defe5c2a77b1eba386141622b02062143a46c88
Ellen <cph-et123@stud.ek.dk>
2026-09-22 12:37:04 +0200

    Logistisk regression

---
+++ Undervisning/Flow 1 - Forretningsforståelse gennem data/Logistisk_regression.R
@@ -0,0 +1,12 @@
+
+library(ISLR2)
+ISLR2
+Default
+glmtest <- glm(default~student+balance+income, family="binomial", data=Default)
+summary(Default$default)
+a.glm <- predict(glmtest, type = ("response"))
+summary(glmtest)
+
+
+glmtest_ny <- glm(default~student+balance, family="binomial", data=Default)
+summary(glmtest_ny)
